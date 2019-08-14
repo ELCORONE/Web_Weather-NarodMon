f809932169d74f22a15683fc55ae2db3bb9b6f63
Илья Корон <46747598+ELCORONE@users.noreply.github.com>
2019-08-14 11:56:54 +1000

    Входящие данные с Ардуино

--- web/query.php
+++ web/query.php
@@ -1,41 +1,40 @@
 <?php
 
-require_once 'config.php'; // Конфигурация подключения к MySQL
-
-//Подключение к MySQL с именем $sql_host под пользователем $sql_user с паролем $sql_pass и выбором базы $sql_base
-$sql_connect = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_base) or die("Ошибка " . mysqli_error($sql_connect));
-$sql_connect->set_charset('utf8'); // Установка кодировки utf8
-
-
+require_once 'config.php';						// Конфигурация подключения к MySQL
 date_default_timezone_set('Asia/Vladivostok');	// Временная зона
-$d = date("Y-m-d");								// Дата
-$t = date("H:i:s");								// Время
+$time = date("H:i");							// Время
 
 // Если пришли значения с ESP8266
 if(!empty($_POST['secretkey']) && $_POST['secretkey'] == "ButterFly140")
     {
+	//Подключение к базе с показаниями за час
+	$sql_connect = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_base) or die("Ошибка " . mysqli_error($sql_connect));
     $mac_address = $_POST['mac_address'];	// Получение MAC-адреса
     $temperature = $_POST['temperature'];	// Получение температуры
     $humidity = $_POST['humidity'];			// Получение влажности
     $pressure = $_POST['pressure'];			// Получение давления
-	$altitude = $_POST['altitude'];			// Получения какой-то хуйни
-	
-	require_once 'sqlsend.php'; // Работа с таблицей
-	
-	require_once 'forecast.php';	// Расчет линии трейдинга по наименьшему углу
-	
+	$altitude = $_POST['altitude'];
+		
+	require_once 'onehour.php';		// Работа с таблицей часа
+	require_once 'oneday.php';		// Работа с таблицей дня
+	require_once 'forecast.php';	// Прогноз погоды за текущий час
+		
 	// Лог файл
-	$data = "$t - ".$temperature." - ".$humidity." - ".$pressure." - ".$dpressure." (".$mapdelta.")\n";
+	$data = "$time / ".$temperature." / ".$humidity." / ".$pressure." / ".$dpressure."\n";
 	$textfile = fopen("log.txt", 'a+') or die("не удалось создать файл");
 	fwrite($textfile, $data);
 	fclose($textfile);
 	
-	//Отправка данных на НародМониторинг (Далее опишу как сделать)
+	//Отправка данных на НародМониторинг
 	/*
+	$datanarod = "#"$mac_address."#ELTempo".;
 	$fp = @fsockopen("tcp://narodmon.ru", 8283, $errno, $errstr);
 	if(!$fp) exit("ERROR(".$errno."): ".$errstr);
 	fwrite($fp, $data);
 	fclose($fp);*/
+	mysqli_close($sql_connect);	// Закрывание соедининия с MySQL
 }
-mysqli_close($sql_connect);	// Закрывание соедининия с MySQL
+else {
+	echo "<center><a href=\"http://milky.space\">Access denied</a></center><br>";
+}
 ?>
