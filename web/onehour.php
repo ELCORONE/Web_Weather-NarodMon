fbcfad3367578f7a306fdf719d658b30cdd330ef
Илья Корон <46747598+ELCORONE@users.noreply.github.com>
2019-08-14 11:55:43 +1000

    Запись в таблицу за час

--- web/onehour.php
+++ web/onehour.php
@@ -1,14 +1,14 @@
 <?php
-//Подключение к базе с показаниями за час
-$sql_connect = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_base) or die("Ошибка " . mysqli_error($sql_connect));
+
 // Запрос всех записей таблицы
 $query_full = "SELECT * FROM $sql_tabl";
 $result_full = $sql_connect->query($query_full);
+
 //Переменные ввиде массива
 $temps = array();
 $hemis = array();
-$pressurs = array();
 $times = array();
+
 //Получение всех значений таблицы
 while($row = mysqli_fetch_array($result_full)) {
 	$temps[] = $row['temp'];
@@ -17,7 +17,8 @@ while($row = mysqli_fetch_array($result_full)) {
 	$times[] = $row['time'];
 	}
 // Обновление данных и запись в таблицу
-for ($i = 1; $i <= 10; $i++){
+for ($i = 1; $i <= 10; $i++)
+	{
 	if($i == 10) {	// Если последний номер - записывать текущие значения в таблицу
 		$temps[$i] = $temperature;
 		$hemis[$i] = $humidity;
@@ -30,10 +31,10 @@ for ($i = 1; $i <= 10; $i++){
 		$pressures[$i] = next($pressures);
 		$times[$i] = next($times);
 	}
+	
 	// Запись данных в таблицу
 	$query_temp = "UPDATE $sql_tabl SET temp=$temps[$i],hemi=$hemis[$i],pressure=$pressures[$i],time='$times[$i]' WHERE id=$i";
 	$result_temp = $sql_connect->query($query_temp);
 	if (!$result_temp) die('updating error'. mysql_error()); // Сообщение об ошибке
 }
-mysqli_close($sql_connect);	// Закрывание соедининия с MySQL
 ?>
