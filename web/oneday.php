2c71a7028eb288a8c99f3d51c648498c395a1ce6
Илья Корон <46747598+ELCORONE@users.noreply.github.com>
2025-09-25 11:33:28 +1000

    Update 2.0

--- web/oneday.php
+++ web/oneday.php
@@ -1,34 +1,56 @@
 <?php
-$sql_connect_day = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_base) or die("Ошибка " . mysqli_error($sql_connect));
-
-$query_day = "SELECT * FROM $sql_day";
-$result_day = $sql_connect_day->query($query_day);
-
-$hpressures = array();
-$hours = array();
-
-while($row = mysqli_fetch_array($result_day)) {
-	$hours[] = $row['hours'];
-	$hpressures[] = $row['pressure'];
-	}
-
-$hour = date("H");
-
-if($hour != $hours[23]) {
-	for ($i = 1; $i <= 24; $i++){
-		if($i == 24) {	// Если последний номер - записывать текущие значения в таблицу
-			$hpressures[$i] = $pressure;
-			$hours[$i] = $hour;
-		}
-		else {			// Текущий = следующий
-			$hpressures[$i] = next($hpressures);
-			$hours[$i] = next($hours);
-		}
-		// Запись данных в таблицу
-		$query_temp_day = "UPDATE $sql_day SET pressure=$hpressures[$i],hours='$hours[$i]' WHERE id=$i";
-		$result_temp = $sql_connect_day->query($query_temp_day);
-		if (!$query_temp_day) die('updating error'. mysql_error()); // Сообщение об ошибке
-	}	
+try {
+    // Проверяем, сменился ли час
+    $lastHour = date('H', $data['times'][8]);
+    $currentHour = date('H', $time);
+    
+    if($lastHour != $currentHour) {
+        // Запрос всех записей таблицы дня
+        $query_full_day = "SELECT * FROM $sql_weatherday ORDER BY id";
+        $stmt_full_day = $pdo->query($query_full_day);
+        $rows_day = $stmt_full_day->fetchAll(PDO::FETCH_ASSOC);
+        
+        // Инициализация массивов
+        $data_day = ['temps' => [], 'pressures' => [], 'times' => []];
+        
+        // Заполнение массивов
+        foreach ($rows_day as $row_day) {
+            $data_day['temps'][] = $row_day['temp'];
+            $data_day['pressures'][] = $row_day['pressure'];
+            $data_day['times'][] = $row_day['time'];
+        }
+        
+        // Расчет средних значений за час
+        $avg_temp_hour = array_sum($data['temps']) / count($data['temps']);
+        $avg_pressures_hour = round(array_sum($data['pressures']) / count($data['pressures']), 2);
+        
+        // Обновление данных за день
+        for ($iday = 0; $iday <= 23; $iday++) {
+            if($iday == 23) {
+                $data_day['temps'][$iday] = $avg_temp_hour;
+                $data_day['pressures'][$iday] = $avg_pressures_hour;
+                $data_day['times'][$iday] = $time;
+            } else {
+                $data_day['temps'][$iday] = $data_day['temps'][$iday + 1];
+                $data_day['pressures'][$iday] = $data_day['pressures'][$iday + 1];
+                $data_day['times'][$iday] = $data_day['times'][$iday + 1];
+            }
+            
+            // Запись данных с подготовленным запросом
+            $update_day = "UPDATE $sql_weatherday SET temp = :temp, 
+                          pressure = :pressure, time = :time WHERE id = :id";
+            
+            $stmt = $pdo->prepare($update_day);
+            $stmt->execute([
+                ':temp' => $data_day['temps'][$iday],
+                ':pressure' => $data_day['pressures'][$iday],
+                ':time' => $data_day['times'][$iday],
+                ':id' => $iday
+            ]);
+        }
+    }
+    
+} catch(PDOException $e) {
+    error_log("Oneday error: " . $e->getMessage());
 }
-mysqli_close($sql_connect_day);	// Закрывание соедининия с MySQL
 ?>
