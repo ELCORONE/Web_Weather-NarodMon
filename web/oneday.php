ba48b8101522b132b7e1b5fcc5e31f064920c6fe
Илья Корон <46747598+ELCORONE@users.noreply.github.com>
2026-02-24 22:37:42 +1000

    Update oneday.php

--- web/oneday.php
+++ web/oneday.php
@@ -11,38 +11,43 @@ try {
         $rows_day = $stmt_full_day->fetchAll(PDO::FETCH_ASSOC);
         
         // Инициализация массивов
-        $data_day = ['temps' => [], 'pressures' => [], 'times' => []];
+        $data_day = ['temps' => [], 'hemis' => [],'pressures' => [], 'times' => []];
         
         // Заполнение массивов
         foreach ($rows_day as $row_day) {
             $data_day['temps'][] = $row_day['temp'];
+			$data_day['hemis'][] = $row_day['hemi'];
             $data_day['pressures'][] = $row_day['pressure'];
             $data_day['times'][] = $row_day['time'];
         }
         
         // Расчет средних значений за час
         $avg_temp_hour = array_sum($data['temps']) / count($data['temps']);
+        $avg_hemi_hour = array_sum($data['hemis']) / count($data['hemis']);
         $avg_pressures_hour = round(array_sum($data['pressures']) / count($data['pressures']), 2);
         
         // Обновление данных за день
         for ($iday = 0; $iday <= 23; $iday++) {
             if($iday == 23) {
                 $data_day['temps'][$iday] = $avg_temp_hour;
+				$data_day['hemis'][$iday] = $avg_hemi_hour;
                 $data_day['pressures'][$iday] = $avg_pressures_hour;
                 $data_day['times'][$iday] = $time;
             } else {
                 $data_day['temps'][$iday] = $data_day['temps'][$iday + 1];
+                $data_day['hemis'][$iday] = $data_day['hemis'][$iday + 1];
                 $data_day['pressures'][$iday] = $data_day['pressures'][$iday + 1];
                 $data_day['times'][$iday] = $data_day['times'][$iday + 1];
             }
             
             // Запись данных с подготовленным запросом
-            $update_day = "UPDATE $sql_weatherday SET temp = :temp, 
+            $update_day = "UPDATE $sql_weatherday SET temp = :temp, hemi = :hemi,
                           pressure = :pressure, time = :time WHERE id = :id";
             
             $stmt = $pdo->prepare($update_day);
             $stmt->execute([
                 ':temp' => $data_day['temps'][$iday],
+				':hemi' => $data_day['hemis'][$iday],
                 ':pressure' => $data_day['pressures'][$iday],
                 ':time' => $data_day['times'][$iday],
                 ':id' => $iday
